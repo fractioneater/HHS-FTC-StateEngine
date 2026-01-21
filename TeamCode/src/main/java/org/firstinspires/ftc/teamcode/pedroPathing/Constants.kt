eb885a2b7d1520c5f37526f771c763ab3093e827
fractioneater <fractioneater@gmail.com>
2026-01-21 16:14:57 -0500

    As much as I can do without the robot (switch to three wheel localizer)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.kt
@@ -5,7 +5,7 @@ import com.pedropathing.follower.FollowerConstants
 import com.pedropathing.ftc.FollowerBuilder
 import com.pedropathing.ftc.drivetrains.MecanumConstants
 import com.pedropathing.ftc.localization.Encoder
-import com.pedropathing.ftc.localization.constants.DriveEncoderConstants
+import com.pedropathing.ftc.localization.constants.ThreeWheelConstants
 import com.pedropathing.paths.PathConstraints
 import com.qualcomm.robotcore.hardware.DcMotorSimple
 import com.qualcomm.robotcore.hardware.HardwareMap
@@ -20,11 +20,11 @@ object Constants {
     return FollowerBuilder(followerConstants, hardwareMap)
       .pathConstraints(pathConstraints)
       .mecanumDrivetrain(driveConstants)
-      .driveEncoderLocalizer(localizerConstants)
+      .threeWheelLocalizer(localizerConstants)
       .build()
   }
 
-  var driveConstants: MecanumConstants? = MecanumConstants()
+  var driveConstants: MecanumConstants = MecanumConstants()
     .maxPower(1.0)
     .rightFrontMotorName("rfD")
     .rightRearMotorName("rbD")
@@ -35,17 +35,17 @@ object Constants {
     .rightFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
     .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE)
 
-  var localizerConstants: DriveEncoderConstants? = DriveEncoderConstants()
-    .rightFrontMotorName("rfD")
-    .rightRearMotorName("rbD")
-    .leftRearMotorName("lbD")
-    .leftFrontMotorName("lfD")
-    .leftFrontEncoderDirection(Encoder.FORWARD)
-    .leftRearEncoderDirection(Encoder.FORWARD)
-    .rightFrontEncoderDirection(Encoder.REVERSE)
-    .rightRearEncoderDirection(Encoder.REVERSE)
-    .robotLength(8.42)
-    .robotWidth(14.33)
-//    .forwardTicksToInches()
-  // TODO NEXT: Forward tuner.
+  var localizerConstants: ThreeWheelConstants = ThreeWheelConstants()
+    .forwardTicksToInches(.001989436789)
+    .strafeTicksToInches(.001989436789)
+    .turnTicksToInches(.001989436789)
+    .leftPodY(5.433) // Done by CAD measurement FOR 6383! TODO: 15317
+    .rightPodY(-5.433) // Done by CAD measurement FOR 6383! Same TODO as above.
+    .strafePodX(-2.5) // TODO! Onshape can't give this one.
+    .leftEncoder_HardwareMapName("lfD") // TODO: the names of these three
+    .rightEncoder_HardwareMapName("rbD") // ...
+    .strafeEncoder_HardwareMapName("rfD") // ...
+    .leftEncoderDirection(Encoder.FORWARD)
+    .rightEncoderDirection(Encoder.FORWARD)
+    .strafeEncoderDirection(Encoder.FORWARD)
 }
